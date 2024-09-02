7235993cd208e7cb80f4d2f9644541e7bbd20ca2
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-09-02 20:13:47 +0900

    Create moji.js

---
+++ moji.js
@@ -0,0 +1,58 @@
+export default [
+    {
+        kanji: "火",
+        fakeLanguage: "フレア",
+        fakeLanguageMeaning: "火の形を象ったシンボル",
+    },
+    {
+        kanji: "矢",
+        fakeLanguage: "矢印",
+        fakeLanguageMeaning: "矢のような形のシンボル",
+    },
+    {
+        kanji: "口",
+        fakeLanguage: "ボックス",
+        fakeLanguageMeaning: "単純な矩形の形状",
+    },
+    {
+        kanji: "乙",
+        fakeLanguage: "カーブ",
+        fakeLanguageMeaning: "スタイライズされた曲線のような形状",
+    },
+    {
+        kanji: "水",
+        fakeLanguage: "ストリーム",
+        fakeLanguageMeaning: "流れる水を表現する形状",
+    },
+    {
+        kanji: "風",
+        fakeLanguage: "ゲイル",
+        fakeLanguageMeaning: "風を象徴する渦巻きの形状",
+    },
+    {
+        kanji: "星",
+        fakeLanguage: "スター",
+        fakeLanguageMeaning: "星型のシンボル",
+    },
+    {
+        kanji: "線",
+        fakeLanguage: "ライン",
+        fakeLanguageMeaning: "平行な線のシリーズ",
+    },
+    {
+        kanji: "円",
+        fakeLanguage: "サークル",
+        fakeLanguageMeaning: "円形のシンボル",
+    },
+    {
+        kanji: "波",
+        fakeLanguage: "ウェーブ",
+        fakeLanguageMeaning: "波のような形状",
+    },
+    {
+        kanji: "点",
+        fakeLanguage: "ドット",
+        fakeLanguageMeaning: "小さな点の形状",
+    }
+    // 他の辞書データもここに追加可能
+];
